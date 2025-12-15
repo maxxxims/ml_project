19fe08a4e3a0ab46382a825b07512dec5762eaea
cnrprod-team-63144 <maxxxi_boyk@mail.ru>
2025-12-15 19:40:27 +0300

    change timeout

--- init-postgres.sh
+++ init-postgres.sh
@@ -2,11 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    -- SELECT 'CREATE DATABASE $POSTGRES_DB'
-    -- WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$POSTGRES_DB')\gexec
-    
-    -- \c $POSTGRES_DB;
-    
     CREATE TABLE IF NOT EXISTS items (
         id BIGSERIAL PRIMARY KEY,
         title TEXT,
@@ -19,10 +14,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
         real_width FLOAT,
         uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
-    
-
-    -- GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $POSTGRES_USER;
-    -- GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_USER;
-    
-    -- SELECT '✅ Table items created successfully' as message;
 EOSQL
